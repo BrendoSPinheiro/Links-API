d5410a58c633fa4e3d255ff2191e84c2bbb79453
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-08-30 23:26:58 -0300

    update querys in userRepository

--- src/app/repositories/UserRepository.js
+++ src/app/repositories/UserRepository.js
@@ -1,36 +1,13 @@
-const { v4 } = require('uuid');
 const Query = require('../../database/index');
 
-let users = [
-  {
-    id: v4(),
-    name: 'Brendo Souza',
-    email: 'brendo@mail.com',
-    password_hash: '123456',
-  },
-  {
-    id: v4(),
-    name: 'Maisa Greice',
-    email: 'maisa@mail.com',
-    password_hash: '654321',
-  },
-  {
-    id: '9519f09e-58dd-473e-8f9b-db187fc27f4f',
-    name: 'Bruno Santos',
-    email: 'bruno@mail.com',
-    password_hash: '$2a$08$RMT6.lv31ASJX53ziv6IU.qOTCJB6qkCaKcqz4TJbDBANfEdwXqnu',
-  },
-];
-
 class UserRepository {
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      users.find((user) => user.id === id),
-    ));
+  async findById(id) {
+    const [user] = await Query.query(`SELECT * FROM users WHERE id = '${id}'`);
+    return user;
   }
 
   async findByEmail(email) {
-    const [user] = await Query.query(`SELECT user FROM users WHERE email = '${email}'`);
+    const [user] = await Query.query(`SELECT * FROM users WHERE email = '${email}'`);
     return user;
   }
 
@@ -43,20 +20,11 @@ class UserRepository {
   }
 
   update(id, { name, email, password_hash }) {
-    return new Promise((resolve) => {
-      const updatedUser = {
-        id,
-        name,
-        email,
-        password_hash,
-      };
-
-      users = users.map((user) => (
-        user.id === id ? updatedUser : user
-      ));
-
-      resolve(updatedUser);
-    });
+    return Query.query(`
+    UPDATE users SET
+      name = '${name}', email = '${email}', password_hash = '${password_hash}'
+      WHERE id = '${id}' RETURNING *
+    `);
   }
 }
 

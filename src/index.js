31df7863b04cfa320314e4b488f79fd0bd0253bc
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-09-01 17:47:41 -0300

    ADD cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 
 app.listen(3000, () => console.log('Server started on port 3000'));

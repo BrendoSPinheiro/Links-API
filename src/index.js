9c0d04ae34fa77f0f4e07c719888c10bc412eedd
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2020-09-03 23:46:07 -0300

    fix: using cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(routes);
 
 app.listen(3000, () => console.log('Server started on port 3000'));
